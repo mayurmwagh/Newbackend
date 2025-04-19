0b434245ab81d65bad5600d216c155347448ee0f
mayurwagh <mayurwagh1412@gmail.com>
2025-04-19 14:22:38 +0530

    user-password added

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,19 +1,24 @@
 pipeline {
     agent any 
+    environment {
+        DOCKER_USERNAME = credentials('docker-username') // Jenkins credential ID
+        DOCKER_PASSWORD = credentials('docker-password') // Jenkins credential ID
+    }
     stages {
-        stage ('pull'){
+        stage('Pull') {
             steps {
                 git branch: 'main', url: 'https://github.com/mayurmwagh/Newbackend.git'
             }
         }
-        stage ('build'){
+        stage('Build') {
             steps {
                 sh 'mvn clean package'
             }
         }
-        stage ('deploy'){
+        stage('Deploy') {
             steps {
                 sh '''
+                    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
                     docker build . -t mayurwagh/cdec-new-backend:latest 
                     docker push mayurwagh/cdec-new-backend:latest
                     docker rmi mayurwagh/cdec-new-backend:latest
@@ -23,3 +28,32 @@ pipeline {
         }
     }
 }
+
+
+
+
+// pipeline {
+//     agent any 
+//     stages {
+//         stage ('pull'){
+//             steps {
+//                 git branch: 'main', url: 'https://github.com/mayurmwagh/Newbackend.git'
+//             }
+//         }
+//         stage ('build'){
+//             steps {
+//                 sh 'mvn clean package'
+//             }
+//         }
+//         stage ('deploy'){
+//             steps {
+//                 sh '''
+//                     docker build . -t mayurwagh/cdec-new-backend:latest 
+//                     docker push mayurwagh/cdec-new-backend:latest
+//                     docker rmi mayurwagh/cdec-new-backend:latest
+//                     kubectl apply -f ./deploy/
+//                 '''
+//             }
+//         }
+//     }
+// }
